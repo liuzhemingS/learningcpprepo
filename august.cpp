55e369fb88c2ae6f0c5a2041a55e155d9911849c
liuzhemings <yxliufeifang@gmail.com>
2026-08-13 14:36:36 +0200

    first commit

---
+++ august.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+int main()
+{
+    std::vector<int> e;
+    
+    for (int i = 0; i<=1000; i++){
+        std::cout << i << "\n\n";
+    }
+
+    return 0;
+}
